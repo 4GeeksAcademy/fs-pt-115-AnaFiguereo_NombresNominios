1031be2918d9419fc73e7e79a30385464e938972
ana <68268688+Angie192020@users.noreply.github.com>
2025-06-24 22:25:19 +0000

    terminado<dominios

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const pronombres = ["the","our"]
+const adjetivos = ["great", "big"];
+const sustantivos = ["jogger", "racoon"];
+
+pronombres.map((pronombre) => {
+  adjetivos.map((adjetivo) => {
+    sustantivos.map((sustantivo) => {
+      console.log(`${pronombre}${adjetivo}${sustantivo}.com`);
+    });
+  });
+})
